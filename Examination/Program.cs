e23389bd598da6492455c8f2724c7c7b57071708
xxxpanda <aman77345@gmail.com>
2021-12-31 12:32:45 +0300

    Created new function Even numbers. This method skip odd numbers and print even numbers.

--- Examination/Program.cs
+++ Examination/Program.cs
@@ -13,3 +13,22 @@ void UserNumber(int[] array)
         array[i] = Convert.ToInt32(Console.ReadLine() + " ");
     }
 }
+void EvenNumbers(int[] array)
+{
+ for (int i = 0; i < array.Length;i++)
+ {
+     if ( array[i]%2 == 0 )
+     {
+        Console.Write(array[i] +" ");
+     }
+ }
+
+}
+
+PrintArr(arr);
+Console.WriteLine();
+UserNumber(arr);
+Console.WriteLine();
+PrintArr(arr);
+Console.WriteLine();
+EvenNumbers(arr);
